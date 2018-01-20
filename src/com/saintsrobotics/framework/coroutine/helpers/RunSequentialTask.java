3f6fe23890e1feafecc846e4e5fdc692abf6a772
Keval Shah <captainrex493@gmail.com>
2018-01-19 16:39:41 -0800

    Updated RS

--- src/com/saintsrobotics/framework/coroutine/helpers/RunSequentialTask.java
+++ src/com/saintsrobotics/framework/coroutine/helpers/RunSequentialTask.java
@@ -1,23 +1,31 @@
 package com.saintsrobotics.framework.coroutine.helpers;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import com.saintsrobotics.framework.coroutine.Task;
+import com.sun.jmx.snmp.tasks.TaskServer;
 
 		
 public abstract class RunSequentialTask extends Task {
-	public RunSequentialTask(Task...tasks ){
-		int tasksLength = tasks.length;
+	private List<Task> tasks;
+
+
+	public RunSequentialTask(Task...tasks){
+		this.tasks = new LinkedList<>();
+	    this.tasks.addAll(Arrays.asList(tasks));
 		
 }
   @Override
   protected void runTask() {
     while (true) {
-      runSequentialy();
-      int tasksLength = tasks.length;
-      for(int TasksRun = 0;TasksRun<tasksLength;TasksRun++) {
-			
-		}
+    	if(tasks.iterator.hasNext()) {
+    		tasks.Waiters = tasks.iterator.next();
+    	}
+}
     }
-  }
+  
 
   /**
    * Runs this method in a while loop. Must wait inside.
