a88a14b22ea638e2460f15942d037adadb6f81d8
dranion <horsenkat@gmail.com>
2018-12-09 15:17:25 -0800

    taskRobot documenting init

--- src/main/java/com/github/dozer/TaskRobot.java
+++ src/main/java/com/github/dozer/TaskRobot.java
@@ -16,7 +16,12 @@ public abstract class TaskRobot extends IterativeRobot {
   protected Task[] disabledTasks;
 
   private TaskRunner runner;
-
+  /**
+   * Initalizes the autonomous state by setting up the TaskRunner for the Robot during Autonomous.
+   * autonmousTasks is expected to be defined in Robot.java and then this declared through super.
+   * Without this declaration, the DriverStation will be warned when this init is called.
+   * @method autonomousInit
+   */
   @Override
   public void autonomousInit() {
     if (this.runner != null) {
@@ -30,6 +35,12 @@ public abstract class TaskRobot extends IterativeRobot {
     runner = new TaskRunner(this.autonomousTasks);
   }
 
+  /**
+   * Initalizes the teleop state by setting up the TaskRunner for the Robot during telop.
+   * teleopTasks is expected to be defined in Robot.java and then this declared through super.
+   * Without this declaration, the DriverStation will be warned when this init is called.
+   * @method teleopInit
+   */
   @Override
   public void teleopInit() {
     if (this.runner != null) {
@@ -43,6 +54,12 @@ public abstract class TaskRobot extends IterativeRobot {
     runner = new TaskRunner(this.teleopTasks);
   }
 
+  /**
+   * Initalizes the test state by setting up the TaskRunner for the Robot during testing.
+   * testTasks is expected to be defined in Robot.java and then this declared through super.
+   * Without this declaration, the DriverStation will be warned when this init is called.
+   * @method testInit
+   */
   @Override
   public void testInit() {
     if (this.runner != null) {
@@ -56,6 +73,12 @@ public abstract class TaskRobot extends IterativeRobot {
     runner = new TaskRunner(this.testTasks);
   }
 
+  /**
+   * Initalizes the disabled state by setting up the TaskRunner for the Robot.
+   * disabledTasks is expected to be defined in Robot.java and then this declared through super.
+   * Without this declaration, the DriverStation will be warned when this init is called.
+   * @method testInit
+   */
   @Override
   public void disabledInit() {
     if (runner != null) {
